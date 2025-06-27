5b88d543fc55c8e48f58ffc94624a42fb0881e4d
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-06-27 15:54:28 +0200

    navBar v1 + pacchetto icone

--- src/app/mainComponents/nav-bar/nav-bar.component.ts
+++ src/app/mainComponents/nav-bar/nav-bar.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  isOpen: boolean | null = null;
 
+  navToggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  get navStateClass(): string | null {
+    if (this.isOpen === true) return 'navExpand';
+    if (this.isOpen === false) return 'navCollapse';
+    return null;
+  }
+
+  close() {
+    this.isOpen = false;
+  }
 }
