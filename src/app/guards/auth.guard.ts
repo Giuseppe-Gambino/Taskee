9796d1d94912db89729db5769f5d4f5d8cbb89d2
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-07-12 20:56:38 +0200

    piccola revisione guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,22 +1,21 @@
 import { inject } from '@angular/core';
 import { CanActivateChildFn, CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { map } from 'rxjs';
+import { filter, map, take } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   return authService.utente$.pipe(
+    filter((user) => user !== undefined),
+    take(1),
     map((user) => {
       if (user === null) {
         router.navigate(['/auth']);
         return false;
-      } else if (user === undefined) {
-        return true;
-      } else {
-        return true;
       }
+      return true;
     })
   );
 };
