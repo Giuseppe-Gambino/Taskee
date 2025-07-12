bfb449f9b00070c99e41cafffeacdc2c262e441c
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-07-12 14:57:45 +0200

    Revert "inizializzazione progetto firebase"
    This reverts commit d25c6e14d6f45eb667a5dc934c481e52c947adcd.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -12,4 +12,5 @@ export const authGuard: CanActivateFn = (route, state) => {
   );
 };
 
+// Per CanActivateChild puoi esportare la stessa funzione (riutilizzo totale):
 export const authGuardChild: CanActivateChildFn = authGuard;
