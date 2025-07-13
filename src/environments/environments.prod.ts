9fe296a71263a1285e7a150016894285afe4e559
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-07-13 19:51:43 +0200

    fix

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAcTOnYk5BBdG3qF6THA25-Pb-jPSGG2Ec',
+    authDomain: 'trello-15762.firebaseapp.com',
+    projectId: 'trello-15762',
+    storageBucket: 'trello-15762.firebasestorage.app',
+    messagingSenderId: '199106836957',
+    appId: '1:199106836957:web:6f3033efbdf7480cff9e34',
+  },
+};
